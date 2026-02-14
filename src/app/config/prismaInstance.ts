0b0f64f6aa9517d72f7a6b3db3832e3c9a54fecf
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-02-15 03:02:22 +0600

    FIX : prima7 postgres adapter for prisma instance

--- src/app/config/prismaInstance.ts
+++ src/app/config/prismaInstance.ts
@@ -1,3 +1,10 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg({ connectionString });
+
+// Instantiate Prisma Client with the adapter
+export const prisma = new PrismaClient({ adapter });
